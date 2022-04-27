a4fa1e06643c30f02c4db122c14b632b13047412
Dingyi Shen <dingyshe@amazon.com>
2022-04-27 11:26:29 -0700

    add uncovered code

---
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/utils/Foo.kt
@@ -0,0 +1,15 @@
+package com.amazonaws.services.chime.sdk.meetings.utils
+
+/**
+ * [Versioning] provides API to retrieve SDK version
+ */
+class Foo {
+    companion object {
+        /**
+         * Return current version of Amazon Chime SDK for Android.
+         */
+        fun foo(): Boolean {
+            return true
+        }
+    }
+}
